8f365c5f1e3aae26e9f9154be3889b1ff8e7387a
Tomislav Dukez <tomdu3@ymail.com>
2024-11-17 21:25:05 +0100

    add category model

--- news/edit/models.py
+++ news/edit/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+class Category(models.Model):
+    """Category model"""
+    name = models.CharField(max_length=150)
+    image = models.ImageField(upload_to="images/")
+
+    def __str__(self):
+        return self.name
