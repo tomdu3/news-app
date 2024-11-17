e0feef1a5903ac6477d8a9ca23329d8db32c6bed
Tomislav Dukez <tomdu3@ymail.com>
2024-11-17 21:58:29 +0100

    add admin views

--- news/edit/admin.py
+++ news/edit/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 from .models import Category, News, Comment
 
 admin.site.register(Category)
-admin.site.register(News)
-admin.site.register(Comment)
+
+class AdminNews(admin.ModelAdmin):
+    list_display = ("title", "category", "created_at", "updated_at")
+    list_display_links = ("title",)
+    search_fields = ("title", "content")
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(News, AdminNews)
+
+class AdminComment(admin.ModelAdmin):
+    list_display = ("name", "email", "status", "created_at")
+    list_display_links = ("name",)
+    list_filter = ("status",)
+    search_fields = ("name", "email", "content")
+admin.site.register(Comment, AdminComment)
