adf8f06587cee3b39af7af566d52811458662249
Tomislav Dukez <tomdu3@ymail.com>
2024-11-17 21:36:18 +0100

    fix remove image from Category class

--- news/edit/models.py
+++ news/edit/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Category(models.Model):
     """Category model"""
     name = models.CharField(max_length=150)
-    image = models.ImageField(upload_to="images/")
 
     class Meta:
         verbose_name_plural = "Categories"
