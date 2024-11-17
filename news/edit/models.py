5202724e106e29bda2f065417ed67310a6c43b0c
Tomislav Dukez <tomdu3@ymail.com>
2024-11-17 21:46:09 +0100

    add news model

--- news/edit/models.py
+++ news/edit/models.py
@@ -9,3 +9,17 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class News(models.Model):
+    """News model
+    """
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150)
+    image = models.ImageField(upload_to="images/")
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
