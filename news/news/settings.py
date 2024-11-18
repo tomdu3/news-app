155464e604cd953e0bb118fb741ef92d080cb0f4
Tomislav Dukez <tomdu3@ymail.com>
2024-11-18 10:23:23 +0100

    add Debug value from environment

--- news/news/settings.py
+++ news/news/settings.py
@@ -25,9 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.getenv("ENVIRONMENT") == "development":
+    DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
