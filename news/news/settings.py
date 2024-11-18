87beb4b89ce6d8a58fb15bce5209dc3f66c6626c
Tomislav Dukez <tomdu3@ymail.com>
2024-11-18 10:26:20 +0100

    fix DEBUG value from comparison

--- news/news/settings.py
+++ news/news/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.getenv("ENVIRONMENT") == "development":
-    DEBUG = True
+DEBUG = os.getenv("ENVIRONMENT") == "development"
+
 
 ALLOWED_HOSTS = ['*']
 
