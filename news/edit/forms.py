c9728d8ce4825a33fcc99940a2014ecc6547572a
Tomislav Dukez <tomdu3@ymail.com>
2024-11-24 06:05:52 +0000

    add autocreate slug from title input

--- news/edit/forms.py
+++ news/edit/forms.py
@@ -1,10 +1,21 @@
 from django import forms
+from django.utils.text import slugify
 from .models import News
 
 class NewsForm(forms.ModelForm):
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+    
     class Meta:
         model = News
-        fields = ['title', 'slug', 'image', 'content', 'category']
+        fields = ['title', 'image', 'content', 'category', 'slug']
         widgets = {
             'image': forms.FileInput(attrs={'required': False}),
+            'title': forms.TextInput(attrs={'class': 'title-input'}),
         }
+        
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        instance.slug = slugify(instance.title)
+        if commit:
+            instance.save()
+        return instance
